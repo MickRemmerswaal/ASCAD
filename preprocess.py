f2dfb10334f12a834e3a05fcd5f9312f5861a865
MickRemmerswaal <mickremmerswaal@gmail.com>
2022-02-16 16:34:18 +0100

    autoencoder implementation changes

--- preprocess.py
+++ preprocess.py
@@ -105,55 +105,8 @@ class SOST_Preprocessor:
 
 class DL_Preprocessor:
     
-    def __init__(self) -> None:
-        self.Encoder = tf.
-        pass
-    
+    def __init__(self, latent_dim, orig_dim) -> None:
+        self.Autoencoder = Autoencoder(latent_dim, orig_dim)    
 
     def preprocess(self, input):
-        return input
-
-class Encoder(tf.keras.layers.Layer):
-    def __init__(self, intermediate_dim):
-        super(Encoder, self).__init__()
-        self.hidden_layer = tf.keras.layers.Dense(
-        units=intermediate_dim,
-        activation=tf.nn.relu,
-        kernel_initializer='he_uniform'
-        )
-        self.output_layer = tf.keras.layers.Dense(
-        units=intermediate_dim,
-        activation=tf.nn.sigmoid
-        )
-    
-    def call(self, input_features):
-        activation = self.hidden_layer(input_features)
-        return self.output_layer(activation)
-
-class Decoder(tf.keras.layers.Layer):
-    def __init__(self, intermediate_dim, original_dim):
-        super(Decoder, self).__init__()
-        self.hidden_layer = tf.keras.layers.Dense(
-            units=intermediate_dim,
-            activation=tf.nn.relu,
-            kernel_initializer='he_uniform'
-        )
-        self.output_layer = tf.keras.layers.Dense(
-            units=original_dim,
-            activation=tf.nn.sigmoid
-        )
-
-    def call(self, code):
-        activation = self.hidden_layer(code)
-        return self.output_layer(activation)
-
-class Autoencoder(tf.keras.Model):
-    def __init__(self, intermediate_dim, original_dim):
-        super(Autoencoder, self).__init__()
-        self.encoder = Encoder(intermediate_dim=intermediate_dim)
-        self.decoder = Decoder(intermediate_dim=intermediate_dim, original_dim=original_dim)
-
-    def call(self, input_features):
-        code = self.encoder(input_features)
-        reconstructed = self.decoder(code)
-        return reconstructed
+        return self.Autoencoder.call(input)
