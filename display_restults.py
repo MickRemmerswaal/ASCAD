9baa157dd81cb86385bc405139ce065734914772
MickRemmerswaal <mickremmerswaal@gmail.com>
2022-03-24 09:27:14 +0100

    added AE & test & results

--- display_restults.py
+++ display_restults.py
@@ -19,28 +19,84 @@ def display_row_data(data_file, row_nr):
     _, axs = plt.subplots(1, sharey=False)
     axs.plot(data_row)
 
+def calc_succes_rate(files, range, index):
+    # files: total results of experiments
+    # range: range of indices which apply to current technique, e.g. range(6) for SOST
+    #        first 5 experiments are bound to SOST
+    # index: denotes amount of attack traces need
+    #        e.g. index = 100 => we check if on index 100 there exists a 0.0 and denote that as a succes
+    rates = []
+    for i in range:
+        selection_rows = np.asarray(files)[:, i]
+        count = np.count_nonzero(selection_rows[:, (index-1)] == 0.0)
+        rate = np.divide(count, 5)
+        rates.append(rate)
+    return rates
+
 total_results = []
 averaged_results = []
 
 for i in range(3,8):
     data_loc = "results_fixed_key_byte"+ str(i) +".csv"
     results = load_data(data_loc)
-    total_results.append(results)
+    np.asarray(total_results.append(results))
 
 # Calculate average scores
-for i in range(5):
-    sost_average = calc_average(total_results, range(6))
-    LDA_average = calc_average(total_results, range(6, 21, 2))
-    PCA_average = calc_average(total_results, range(7, 22, 2))
-sost_labels = ["2", "4", "10", "20", "50", "100"]
-for i in range(0, len(sost_average)):
-    plt.plot(sost_average[i], label=(sost_labels[i] +" POIs"))
+sost_average = calc_average(total_results, range(6))
+LDA_average = calc_average(total_results, range(6, 21, 2))
+PCA_average = calc_average(total_results, range(7, 22, 2))
 
-plt.title("Averege Guessing Entropy of LDA Point of Interest Selection with Variable N POIs")
+
+
+# Calculating Succes rate
+
+succes_rate_indices = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+
+SOST_succes_rate = []
+PCA_succes_rate = []
+LDA_succes_rate = []
+AE_succes_rate = []
+
+# Check for each index and each technique
+# what % of the runs were able to have their guessing entropy at 0
+for index in succes_rate_indices:
+    # SOST
+    SOST_succes_rate.append(calc_succes_rate(total_results, range(6), index))
+    # PCA
+    PCA_succes_rate.append(calc_succes_rate(total_results, range(7, 22, 2), index))
+    # LDA
+    LDA_succes_rate.append(calc_succes_rate(total_results,  range(6, 21, 2), index  ))
+
+
+total_results = []
+averaged_results = []
+
+for i in range(3,8):
+    data_loc = "results_AE_fixed_key_byte"+ str(i) +".csv"
+    results = load_data(data_loc)
+    total_results.append(results)
+
+AE_average = calc_average(total_results, range(6))
+
+sost_labels = ["2", "4", "10", "20", "50", "100"]
+
+for i in succes_rate_indices:
+    # AE Success rate
+    AE_succes_rate.append(calc_succes_rate(total_results, range(6), index))
+
+
+'''
+# Plot results
+for i in range(0, len(LDA_average)):
+    plt.plot(LDA_average[i], label=(str(i+1) +" POIs"), linewidth=2)
+
 plt.ylim([0, 256])
 plt.xlim([0, 1000])
+plt.ylabel("Guessing entropy")
+plt.xlabel("Number of attack traces")
 plt.legend()
 plt.show(block=True)
 
 
+'''
 
